3b5a828568fd17a28fc6212af30364fc7b2df988
jeffer-cmd <cuentajeff000@gmail.com>
2026-08-11 22:40:20 -0500

    ajuste filtro en ventas

--- public/filtro_ventas.js
+++ public/filtro_ventas.js
@@ -6,12 +6,13 @@ document.getElementById("buscador").addEventListener("input", function () {
 
     filas.forEach(fila => {
 
-        let vendedor = fila.children[1].textContent.toLowerCase(); // columna nombre
-        let metodoPago = fila.children[2].textContent.toLowerCase();  // columna email
-        let cliente = fila.children[3].textContent.toLowerCase();  // columna rol
-        let estado = fila.children[6].textContent.toLowerCase();  // columna verificar cuenta
+        let vendedor = fila.children[1].textContent.toLowerCase(); 
+        let metodoPago = fila.children[2].textContent.toLowerCase();  
+        let cliente = fila.children[3].textContent.toLowerCase();  
+        let fecha = fila.children[5].textContent.toLowerCase();  
+        let estado = fila.children[6].textContent.toLowerCase();  
 
-        if (vendedor.includes(valor) || metodoPago.includes(valor) || cliente.includes(valor) || estado.includes(valor)) {
+        if (vendedor.includes(valor) || metodoPago.includes(valor) || cliente.includes(valor) || estado.includes(valor)|| fecha.includes(valor)) {
             fila.style.display = "";
         } else {
             fila.style.display = "none";
