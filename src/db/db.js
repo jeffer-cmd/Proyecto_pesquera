77f635e9b3371922ec6b86d5cc730d60ea17fb28
jeffer-cmd <cuentajeff000@gmail.com>
2026-08-11 22:27:08 -0500

    ajuste conexion

--- src/db/db.js
+++ src/db/db.js
@@ -29,7 +29,11 @@ if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not defined");
 }
 
-const client = postgres(process.env.DATABASE_URL);
+const client = postgres(process.env.DATABASE_URL,{
+    ssl: "require",
+
+  }
+);
 
 const db = drizzle(client);
 
